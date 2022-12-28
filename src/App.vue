c0f49a2220d04b1e724a5f39b35884dc908dcf24
ican-graphics-01 <lankaican@gmail.com>
2022-12-28 11:58:05 +0530

    new header create

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <v-app>
-   
+
     <v-main>
+      <Header>
+        <Navbar />
+      </Header>
+
       <!-- hero section -->
       <hero-section /> 
 
@@ -25,19 +29,25 @@
 </template>
 
 <script>
+
 import CounterView from './components/CounterView.vue';
 import FeaturedTwo from './components/Featured-two.vue';
 import FeaturedView from './components/FeaturedView.vue';
 import FooterView from './components/FooterView.vue';
 import HeroSection from './components/HeroSection.vue';
+import Navbar from './components/Navbar.vue';
 import PortfolioView from './components/PortfolioView.vue';
 export default {
-  components: { HeroSection, FeaturedView, CounterView, FeaturedTwo, PortfolioView, FooterView },
+  components: { HeroSection, FeaturedView, CounterView, FeaturedTwo, PortfolioView, FooterView, Navbar },
   name: 'App',
    
   };
 </script>
 
 <style>
- 
+  header {
+    width: 100vw;
+    background-color: #222;
+    padding: 15px;
+  }
 </style>
